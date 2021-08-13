fe023d0e00334255841501d941a693fb598661b0
Nimmi231 <70900623+Nimmi231@users.noreply.github.com>
2021-08-13 12:29:11 +0530

    create Longest consecutive subsequence.py

---
+++ Array/Longest consecutive subsequence.py
@@ -0,0 +1,48 @@
+class Solution:
+    
+    # arr[] : the input array
+    # N : size of the array arr[]
+    
+    #Function to return length of longest subsequence of consecutive integers.
+    def findLongestConseqSubseq(self,arr, N):
+        arr.sort()
+        bag = []
+        count = 0
+        for i in range(len(arr)-1):
+            if arr[i]+1==arr[i+1]:
+                
+                count+=1
+                bag.append(arr[i])
+                continue
+            else:
+                break
+        return len(bag)+1
+                
+                
+        #code here
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__ == '__main__':
+    t = int(input())
+    for tt in range(t):
+        n=int(input())
+        a = list(map(int, input().strip().split()))
+        print(Solution().findLongestConseqSubseq(a,n))
+# } Driver Code Ends
